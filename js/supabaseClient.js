b0654403390381d5d68dcaf930b3209d65f73fcd
jiibarra-lgtm <jiibarra@ipcac.edu.ar>
2026-09-15 22:52:31 -0300

    asdfasdf

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://klqlmmnwzouwznfpbvfm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtscWxtbW53em91d3puZnBidmZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzOTczMjksImV4cCI6MjEwMzk3MzMyOX0.4AwRxo2kA5WmNaQn2_Ri2AnHWPWItpt_7DQ9ILIEKMY";
+const SUPABASE_URL = "https://rzaynyxqgczltvjsjqvq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6YXlueXhxZ2N6bHR2anNqcXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MTAzMDEsImV4cCI6MjEwNTA4NjMwMX0.4JmaSFZeOmbEL1y1uRLrFTGutchEygAbo72pHTMwfNA";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
